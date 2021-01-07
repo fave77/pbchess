059c6c42f59ba676551519d44a85827a08bd04d2
pbiswas101 <pbiswas101b@gmail.com>
2021-01-07 23:30:12 +0530

    update home page :airplane:

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -3,13 +3,18 @@ import React, { Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 
+import { Player } from '@lottiefiles/react-lottie-player';
+
 import MagnusCarlsen from '../../images/magnus-carlsen.jpg';
 import JuditPolgar from '../../images/judit-polgar.jpg';
 import GarryKaspaov from '../../images/garry-kasparov.jpg';
 
 import './home.css';
 import homebg from '../../images/homebg.svg';
-import homeblob from '../../images/homeblob.gif';
+import chessboard from  '../../images/chessboard.png';
+import homecircles from '../../images/homecircles.json';
+import homeblobs from '../../images/homeblobs.json';
+
 
 class Home extends Component {
   constructor(props) {
@@ -26,50 +31,77 @@ class Home extends Component {
           id = 'homebg'
         />
         <img
-          src = { homeblob }
-          alt = 'Homepage Background Top'
-          id = 'homeblob'
+          src = { chessboard }
+          alt = 'chessboard'
+          id = 'chessboard'
         />
-        <div className = 'p-3 mb-2 text-white'>
-          <h1 className = 'text-center'> pbchess </h1>
-					<h3 className = 'text-center'> An open-sourced free online chess platform </h3>
+        <Player
+          autoplay
+          loop
+          src = { homecircles }
+          style = {{
+            height: '70%',
+            width: '70%',
+            position: 'absolute',
+            marginTop: '20%',
+            zIndex: -1
+          }}
+        >
+        </Player>
+        <Player
+          autoplay
+          loop
+          src = { homeblobs }
+          style = {{
+            height: '10em',
+            width: '10em',
+            position: 'absolute',
+            marginTop: '40%',
+            marginLeft: '55%',
+            zIndex: -1
+          }}
+        >
+        </Player>
+        <div className = 'p-3 mb-2 text-white title'>
+          <h1> pbchess </h1>
+					<h3> An open-sourced free online chess platform </h3>
         </div>
         <Carousel className = 'text-center'>
           <Carousel.Item>
-            <Card>
+            <Card bsPrefix = 'mycard' >
               <Card.Img variant = 'top' src = { MagnusCarlsen } alt = 'Magnus Carlsen' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
               <Card.Body>
                 <Card.Title>
-                Magnus Carlsen
+                  Magnus Carlsen
                 </Card.Title>
-                <Card.Text className = 'text-secondary'>
-                Some people think that if their opponent plays a beautiful game, it’s OK to lose. I don’t. You have to be merciless.
+                <Card.Text>
+                  <q>Some people think that if their opponent plays a beautiful game, it’s OK to lose. I don’t. You have to be merciless.</q>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Carousel.Item>
           <Carousel.Item>
-            <Card>
+            <Card bsPrefix = 'mycard' >
               <Card.Img variant = 'top' src = { JuditPolgar } alt = 'Judit Polgar' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
               <Card.Body>
                 <Card.Title>
-                Judit Polgar
+                  Judit Polgar
                 </Card.Title>
-                <Card.Text className = 'text-secondary'>
-                One can say that in the last decades chess has become more of a sport than of a science. I see it from an artistic point of view.
+                <Card.Text>
+                  <q>One can say that in the last decades chess has become more of a sport than of a science. I see it from an artistic point of view.</q>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Carousel.Item>
           <Carousel.Item>
-            <Card style = {{ width: '75%', maxWidth: '700px' }}>
+            <Card bsPrefix = 'mycard' >
               <Card.Img variant = 'top' src = { GarryKaspaov } alt = 'Garry Kasparov' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
               <Card.Body>
                 <Card.Title>
-                Garry Kasparov
+                  Garry Kasparov
                 </Card.Title>
-                <Card.Text className = 'text-secondary'>
-                To become good at anything you have to know how to apply basic principles. To become great at it, you have to know when to violate those principles.
+                <Card.Text>
+                  <q>To become good at anything you have to know how to apply basic principles. To become great at it, you have to know when to violate those principles.</q>
                 </Card.Text>
               </Card.Body>
             </Card>
