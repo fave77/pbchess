73a8e7cc1cb3fa60facdab7b8f488547181a3ae8
Rishabh Jain <krackerz.pec@gmail.com>
2021-03-10 13:29:37 +0530

    Fixed #5 : Added the Footer to the Homepage (#31)
    * Added the Footer to the Homepage
    
    * Updated Footer to stick at the bottom
    
    * Fixed issue regarding the right margin

---
+++ client/src/components/footer/footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer id="footer">
+        <div className="footer-row">
+          <div className="col-lg-12 col-md-12 col-sm-12  col-xs-12">
+            <div className="item">Made with ❤️ for the chess community</div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-6  col-xs-12">
+            <div className="item">
+              <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">
+                GPLv3 License
+              </a>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-6  col-xs-12">
+            <div className="item">
+              <a href="https://github.com/fave77/pbchess">Support Us</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default Footer;
