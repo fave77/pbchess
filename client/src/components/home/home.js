2ae01b5d1a757507b328973ed17e7c045f08ee1c
Rishabh Jain <krackerz.pec@gmail.com>
2021-03-10 13:29:37 +0530

    Fixed #5 : Added the Footer to the Homepage (#31)
    * Added the Footer to the Homepage
    
    * Updated Footer to stick at the bottom
    
    * Fixed issue regarding the right margin

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -14,6 +14,7 @@ import homebg from '../../images/homebg.svg';
 import chessboard from  '../../images/chessboard.png';
 import homeblobs from '../../images/homeblobs.json';
 
+import Footer from "../footer/footer";
 
 class Home extends Component {
   constructor(props) {
@@ -23,89 +24,92 @@ class Home extends Component {
   render() {
 
     return (
-      <div>
-        <img
-          src = { homebg }
-          alt = 'Homepage Background Top'
-          id = 'homebg'
-        />
-        <img
-          src = { chessboard }
-          alt = 'chessboard'
-          id = 'chessboard'
-        />
-        <Player
-          autoplay
-          loop
-          src = { homeblobs }
-          style = {{
-            height: '50%',
-            width: '50%',
-            position: 'absolute',
-            marginTop: '15%',
-            zIndex: -1
-          }}
-        >
-        </Player>
-        <Player
-          autoplay
-          loop
-          src = { homeblobs }
-          style = {{
-            height: '10em',
-            width: '10em',
-            position: 'absolute',
-            marginTop: '550px',
-            marginLeft: '55%',
-            zIndex: -1
-          }}
-        >
-        </Player>
-        <div className = 'p-3 mb-2 text-white title'>
-          <h1> pbchess </h1>
-					<h3> An open-sourced free online chess platform </h3>
+      <div id="page-container">
+        <div id="content-wrap">
+          <img
+            src = { homebg }
+            alt = 'Homepage Background Top'
+            id = 'homebg'
+          />
+          <img
+            src = { chessboard }
+            alt = 'chessboard'
+            id = 'chessboard'
+          />
+          <Player
+            autoplay
+            loop
+            src = { homeblobs }
+            style = {{
+              height: '50%',
+              width: '50%',
+              position: 'absolute',
+              marginTop: '15%',
+              zIndex: -1
+            }}
+          >
+          </Player>
+          <Player
+            autoplay
+            loop
+            src = { homeblobs }
+            style = {{
+              height: '10em',
+              width: '10em',
+              position: 'absolute',
+              marginTop: '550px',
+              marginLeft: '55%',
+              zIndex: -1
+            }}
+          >
+          </Player>
+          <div className = 'p-3 mb-2 text-white title'>
+            <h1> pbchess </h1>
+            <h3> An open-sourced free online chess platform </h3>
+          </div>
+          <Carousel className = 'text-center'>
+            <Carousel.Item>
+              <Card bsPrefix = 'mycard' >
+                <Card.Img variant = 'top' src = { MagnusCarlsen } alt = 'Magnus Carlsen' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
+                <Card.Body>
+                  <Card.Title>
+                    Magnus Carlsen
+                  </Card.Title>
+                  <Card.Text>
+                    <q>Some people think that if their opponent plays a beautiful game, it’s OK to lose. I don’t. You have to be merciless.</q>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card bsPrefix = 'mycard' >
+                <Card.Img variant = 'top' src = { JuditPolgar } alt = 'Judit Polgar' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
+                <Card.Body>
+                  <Card.Title>
+                    Judit Polgar
+                  </Card.Title>
+                  <Card.Text>
+                    <q>One can say that in the last decades chess has become more of a sport than of a science. I see it from an artistic point of view.</q>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Card bsPrefix = 'mycard' >
+                <Card.Img variant = 'top' src = { GarryKaspaov } alt = 'Garry Kasparov' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
+                <Card.Body>
+                  <Card.Title>
+                    Garry Kasparov
+                  </Card.Title>
+                  <Card.Text>
+                    <q>To become good at anything you have to know how to apply basic principles. To become great at it, you have to know when to violate those principles.</q>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Carousel.Item>
+          </Carousel>
         </div>
-        <Carousel className = 'text-center'>
-          <Carousel.Item>
-            <Card bsPrefix = 'mycard' >
-              <Card.Img variant = 'top' src = { MagnusCarlsen } alt = 'Magnus Carlsen' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
-              <Card.Body>
-                <Card.Title>
-                  Magnus Carlsen
-                </Card.Title>
-                <Card.Text>
-                  <q>Some people think that if their opponent plays a beautiful game, it’s OK to lose. I don’t. You have to be merciless.</q>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Card bsPrefix = 'mycard' >
-              <Card.Img variant = 'top' src = { JuditPolgar } alt = 'Judit Polgar' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
-              <Card.Body>
-                <Card.Title>
-                  Judit Polgar
-                </Card.Title>
-                <Card.Text>
-                  <q>One can say that in the last decades chess has become more of a sport than of a science. I see it from an artistic point of view.</q>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Carousel.Item>
-          <Carousel.Item>
-            <Card bsPrefix = 'mycard' >
-              <Card.Img variant = 'top' src = { GarryKaspaov } alt = 'Garry Kasparov' style = {{ width: '50%', margin: 'auto' }} className = 'rounded-circle' />
-              <Card.Body>
-                <Card.Title>
-                  Garry Kasparov
-                </Card.Title>
-                <Card.Text>
-                  <q>To become good at anything you have to know how to apply basic principles. To become great at it, you have to know when to violate those principles.</q>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Carousel.Item>
-        </Carousel>
+        <Footer />
       </div>
     );
   }
