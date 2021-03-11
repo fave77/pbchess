b51012b487afd1cc58f5504ad981a94f9a9ffda5
Arshansh <arshanshagarwal1@gmail.com>
2021-03-11 23:29:15 +0530

    added Learning section

--- client/src/components/learn/learn.js
+++ client/src/components/learn/learn.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import Videos from '../videos/videos';
-import './learn.css';
+import React, { Component } from "react";
+import Terms from "./Terms.json";
+import "./learn.css";
 
-const Learn=() => {
-  return (
-    <Videos />
-  )
-};
+export class Learn extends Component {
+  render() {
+    console.log(Terms);
+    return (
+      <div className="content">
+        <div className="Container">
+          <p className="heading">Learning Chess</p>
+          <div className="Terms">
+            <div className="tags">
+              <h4>
+                <a href="#A">A</a>-<a href="#B">B</a>-<a href="#c">C</a>-
+                <a href="#D">D</a>-<a href="#E">E</a>-<a href="#F">F</a>-
+                <a href="#G">G</a>-<a href="#H">H</a>-<a href="#I">I</a>-
+                <a href="#J">J</a>-<a href="#K">K</a>-<a href="#L">L</a>-
+                <a href="#M">M</a>-<a href="#N">N</a>-<a href="#O">O</a>-
+                <a href="#P">P</a>-<a href="#Q">Q</a>-<a href="#R">R</a>-
+                <a href="#S">S</a>-<a href="#T">T</a>-<a href="#U">U</a>-
+                <a href="#V">V</a>-<a href="#W">W</a>-<a href="#X">X</a>-
+                <a href="#Y">Y</a>-<a href="#Z">Z</a>
+              </h4>
+              -
+            </div>
+            {Terms.terms.map((item, i) => (
+              <div className="block" id={`${item.id}`}>
+                <div className="title">
+                  <h4>{item.title}</h4>
+                </div>
+                <div className="meaning">{item.meaning}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Learn;
