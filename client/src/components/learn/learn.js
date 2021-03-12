219427d1fc9acc89eb4459bb2e5653262371ecce
Arshansh <arshanshagarwal1@gmail.com>
2021-03-12 12:31:43 +0530

    After creating Vocab and resolving said changes

--- client/src/components/learn/learn.js
+++ client/src/components/learn/learn.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import Videos from '../videos/videos';
-import './learn.css';
+import React, { Component } from "react";
+import Videos from "../videos/videos";
+import Vocab from "../Vocab/vocab";
+import "./learn.css";
 
-const Learn=() => {
-  return (
-    <Videos />
-  )
-};
+export class Learn extends Component {
+  render() {
+    return (
+      <>
+        <Vocab />
+        <Videos />
+      </>
+    );
+  }
+}
 
 export default Learn;
