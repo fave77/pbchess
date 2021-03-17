317202c3c3683270b7ac0d453470cadd4d1ea58a
Hrishabh Joshi <joshi008@users.noreply.github.com>
2021-03-17 23:59:56 +0530

    contributor card hover

--- client/src/components/contrib/Contrib.js
+++ client/src/components/contrib/Contrib.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import ContribCard from './ContribCard.js'
+import { useSpring, animated, useTransition } from 'react-spring'
 
 import Footer from "../footer/footer";
 
 function Contrib() {
     const [data, setData] = useState([]);
     const url = "https://api.github.com/repos/fave77/pbchess/contributors"
+    const styl = useSpring({ opacity: 1, from: { opacity: 0 } })
+
 
     const fetchURL = async () => {
         const response = await fetch(url);
@@ -17,6 +20,12 @@ function Contrib() {
         fetchURL();
     }, [])
 
+    const [show, set] = useState(false)
+    const transitions = useTransition(show, null, {
+        from: { position: 'absolute', opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
 
     return (
         <div className="page_container">
@@ -24,11 +33,11 @@ function Contrib() {
             <br />
             <br />
 
-            <div className="space">
+            <animated.div style={styl} className="space">
                 {data.map(({ avatar_url, url, login, html_url }) => (
                     <ContribCard avatar={avatar_url} url={url} login={login} html_url={html_url}></ContribCard>
                 ))}
-            </div>
+            </animated.div>
             <br></br>
             <Footer />
         </div>
--- client/src/components/contrib/ContribCard.js
+++ client/src/components/contrib/ContribCard.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react'
 import "./contrib.css"
 import Image from 'react-bootstrap/Image'
 import { Link } from 'react-router-dom';
+import { useSpring, animated, useTransition } from 'react-spring'
+import { CSSTransition } from 'react-transition-group';
+
+
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 30, (x - window.innerWidth / 2) / 30, 1.03]
+const trans = (x, y, s) => `scale(${s})`
 
 function ContribCard(props) {
 
+    const [p, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
     const onclickHandler = (e) => {
         window.location = props.html_url
     }
@@ -24,13 +31,22 @@ function ContribCard(props) {
 
 
     return (
-        <div onClick={onclickHandler}>
+        <animated.div onClick={onclickHandler}
+            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+            onMouseLeave={() => set({ xys: [0, 0, 1] })}
+            style={{ transform: p.xys.interpolate(trans) }}
+        >
+
+
+
             <span className="boundary">
                 <Image src={props.avatar} roundedCircle responsive className="img" />
                 {data == [] ? "unknown" : data.name}
                 <h3>{props.login}</h3>
             </span>
-        </div>
+
+
+        </animated.div>
     )
 }
 
