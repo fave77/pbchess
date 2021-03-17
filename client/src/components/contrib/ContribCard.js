8f284fb6062d24301945546b83d16e971563ed78
Hrishabh Joshi <joshi008@users.noreply.github.com>
2021-03-18 01:48:37 +0530

    contributor card active | no select

--- client/src/components/contrib/ContribCard.js
+++ client/src/components/contrib/ContribCard.js
@@ -4,16 +4,20 @@ import Image from 'react-bootstrap/Image'
 import { Link } from 'react-router-dom';
 import { useSpring, animated, useTransition } from 'react-spring'
 import { CSSTransition } from 'react-transition-group';
-
+import ProfileCard from "./ProfileCard"
+import FadeIn from 'react-fade-in';
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 30, (x - window.innerWidth / 2) / 30, 1.03]
 const trans = (x, y, s) => `scale(${s})`
 
 function ContribCard(props) {
 
-    const [p, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+    const [p, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 1, tension: 200, friction: 30 } }))
+    const [loading, setload] = useState(true)
+    const [ccolor, setColor] = useState('#151922');
+
     const onclickHandler = (e) => {
-        window.location = props.html_url
+        // window.location = props.html_url
     }
 
     const [data, setData] = useState([]);
@@ -23,30 +27,50 @@ function ContribCard(props) {
         const response = await fetch(url);
         const jsonData = await response.json();
         setData(jsonData)
+        setload(false)
     }
 
     useEffect(() => {
         fetchURL();
     }, [])
 
+    const mouseDownHandler = (e) => {
+        e.style = { color: 'black' }
+    }
+
 
     return (
-        <animated.div onClick={onclickHandler}
-            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-            onMouseLeave={() => set({ xys: [0, 0, 1] })}
-            style={{ transform: p.xys.interpolate(trans) }}
-        >
+        <React.Fragment>
+            {loading ? (
+                <ProfileCard className="boundary" />
+            ) : (
+                <FadeIn>
+                    <animated.div onClick={onclickHandler}
+                        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                        style={{ transform: p.xys.interpolate(trans) }}
+                    >
 
 
+                        <a href={props.html_url} target="_blank">
+                            <span style={{ backgroundColor: ccolor }}
+                                className="boundary" onMouseDown={() => { setColor('#1a202b') }}
+                                onMouseUp={() => { setColor('#151922') }}
+                                onMouseLeave={() => { setColor('#151922') }}>
 
-            <span className="boundary">
-                <Image src={props.avatar} roundedCircle responsive className="img" />
-                {data == [] ? "unknown" : data.name}
-                <h3>{props.login}</h3>
-            </span>
+                                <Image src={props.avatar} roundedCircle responsive className="img" />
+                                {data.name == null ? <br /> : data.name}
+                                <h3>{props.login}</h3>
+                            </span>
+                        </a>
+
+                    </animated.div>
+                </FadeIn>
+            )
+            }
 
 
-        </animated.div>
+        </React.Fragment >
     )
 }
 
