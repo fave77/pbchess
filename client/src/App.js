bb3c681e741c130a715f39941867f9346bf8fc11
Hrishabh Joshi <54844760+joshi008@users.noreply.github.com>
2021-03-20 01:46:09 +0530

    contributor's page (#72)

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,9 @@ import Profile from './components/profile/profile';
 import Lobby from './components/lobby/lobby';
 import Confirm from './components/confirm/confirm';
 import NotFound from './components/pagenotfound/page-not-found';
+import Contrib from './components/contrib/contrib.js';
 import ChangePassword from './components/password/password';
+
 
 import PrivateRoute from './components/private/private';
 
@@ -63,6 +65,7 @@ class App extends Component {
             <Route exact path = '/login' component = { Login } />
             <Route exact path = '/confirm' component = { Confirm } />
             <Route exact path = '/register' component = { Register } />
+            <Route exact path = '/contributors' component = { Contrib } />
             <Route path = '/@/:profileId' component = { Profile } />
             <PrivateRoute path = '/password' component = { ChangePassword } />
             <PrivateRoute path = '/play' component = { Lobby } />
