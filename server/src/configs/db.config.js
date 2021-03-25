ca2bd0fc905ec08ffd0f68a44d221ae7c33e1991
ABHAY CHAUHAN <42849043+abhay27chauhan@users.noreply.github.com>
2021-03-25 16:56:34 +0530

    inital connection error handled (#78)

--- server/src/configs/db.config.js
+++ server/src/configs/db.config.js
@@ -4,17 +4,21 @@ const { connect, connection } = require('mongoose');
 const prodMongoURI = process.env.PROD_DATABASE_URI || '';
 const devMongoURI = process.env.DEV_DATABASE_URI || '';
 
-const configDB = _ => {
+const configDB = async _ => {
   const mongoURI = (process.env.NODE_ENV === 'production')
     ? prodMongoURI
     : devMongoURI;
 
-  connect(mongoURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+  try {
+    await connect(mongoURI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+          });
+  } catch (error) {
+    console.error("Database Initial Connection Error!\n", error);
+  }
 
   connection.once('open', _ =>
     console.log('Database connected ==> ', mongoURI)
