52a4651d7f34ab17853a54c18695a0229f0f64e7
aanchal <fatwaniaanchal@gmail.com>
2021-04-05 00:14:58 +0530

    fix(learn): added tabs and player size fix

--- client/src/components/learn/learn.js
+++ client/src/components/learn/learn.js
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Tab, Tabs } from "react-bootstrap";
 import Videos from "../videos/videos";
 import Vocab from "../vocab/vocab";
 import "./learn.css";
 
 const Learn = () => {
+  const [currentTab, setCurrentTab] = useState("Vocabulary");
+
   return (
     <div>
-      <Vocab />
-      <Videos />
+      <Tabs
+        activeKey={currentTab}
+        onSelect={(t) => setCurrentTab(t)}
+        className="learn-tabs"
+      >
+        <Tab eventKey="Vocabulary" title="Vocabulary" tabClassName="learn-tab">
+          <Vocab />
+        </Tab>
+        <Tab eventKey="Videos" title="Videos" tabClassName="learn-tab">
+          <Videos />
+        </Tab>
+      </Tabs>
     </div>
   );
 };
