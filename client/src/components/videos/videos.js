8e22dfe5dfd1eeb12d003ec77dcd62aa1d13921a
Aanchal <63228776+aanchal-fatwani@users.noreply.github.com>
2021-04-05 12:51:42 +0530

    fix(learn): added tabs and player size fix (#90)

--- client/src/components/videos/videos.js
+++ client/src/components/videos/videos.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './videos.css';
 const Videos=() => {
+    const availableHt = window.innerHeight;
     return (
         <div className='chessBoard'>
-            <iframe src='https://lichess.org/tv/frame?theme=blue-marble&bg=dark' style={{width: '800px', height: '800px'}} allowtransparency='true' frameborder='0'></iframe>
+            <iframe src='https://lichess.org/tv/frame?theme=blue-marble&bg=dark' style={{width: availableHt - 180, height: availableHt - 150}} allowtransparency='true' frameborder='0'></iframe>
         </div>
     );
 };
